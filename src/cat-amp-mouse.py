95f382f699fa5adcb656c61850592c78210df4e4
Cragady <pinkdotdreamer@gmail.com>
2019-05-09 13:00:03 -0600

    removed game_vars from main program

--- src/cat-amp-mouse.py
+++ src/cat-amp-mouse.py
@@ -1,4 +1,4 @@
-import pygame, sys, os, random, math, winsound, time
+import pygame, sys, os, random, math, winsound, time, game_vars
 from pygame.locals import *
 from threading import Timer
 
@@ -25,188 +25,6 @@ else:
 w, h = pygame.display.get_surface().get_size()
 background_image = pygame.image.load(pather('./assets/neon-tun-back.png')).convert()
 background_image = pygame.transform.scale(background_image, (w, h))
-
-
-class game_vars:
-        #Enemy Costumes
-    def __init__(self):
-        self.costumes = [
-            {
-                'img': 'bmouse.png',
-                'size': [40, 65]
-            },
-            {
-                'img': 'b-c.png',
-                'size': [40, 65]
-            },
-            {
-                'img': 'b-a.png',
-                'size': [40, 65]
-            },
-            {
-                'img': 'b-t.png',
-                'size': [40,65]
-            }
-        ]
-
-        #Game Vars
-        self.level = 1
-        self.score = 0
-        self.lives = 10
-        self.speed_enem = 1
-        self.speed_all = 1 
-        self.firing = True
-        self.keys = {}
-        self.game_loop = True
-        self.music = True
-        self.once = True
-        self.messgo = True
-        self.first_time = True
-        self.firstxt = True
-        self.secondtxt = False
-        self.thirdtxt = False
-        self.sscreen = True
-        self.pause_time = 0
-        self.last_pause = 0
-        self.mw_trans = 2.8
-        self.paused = False
-        self.messcolor = [0, 0, 0]
-        self.messfont = pygame.font.SysFont('Arial', 30)
-        self.mess_surface = pygame.Surface((300, 100))
-        self.myfont = pygame.font.SysFont('Arial', 20)
-        self.text_surface = pygame.Surface((100,75))
-
-
-    #Score Watching
-    def score_watcher(self, sc):
-        if sc < 20:
-            return 1
-        elif sc >= 20 and sc <= 29:
-            return 2
-        elif sc >= 30 and sc <= 49:
-            return 3
-        elif sc >= 50:
-            return 4
-
-    def show_message(self):
-        if self.messgo:
-            if self.first_time:
-                self.first_time = False
-                Timer(0.2, self.message_timer, [20]).start()
-            if self.firstxt:
-                self.messcolor = [255, 255, 255]
-            elif self.secondtxt:
-                self.messcolor = [255, 153, 229]
-            elif self.thirdtxt:
-                self.messcolor = [0, 255, 255]
-            message = self.messfont.render('Automatic Fire Mode', False, self.messcolor)
-            spaceage = self.messfont.render('(Hold Spacebar)', False, self.messcolor)
-            tw, th = message.get_rect().size
-            sw, sh = spaceage.get_rect().size
-            mw, mh = self.mess_surface.get_rect().size
-            
-            
-            self.mess_surface.fill((0, 0, 0))
-            self.mess_surface.blit(message, (mw / 2 - tw / 2, mh * 0.25 - th * 0.25))
-            self.mess_surface.blit(spaceage, (mw * 0.5 - sw * 0.5, mh * 0.75 - sh * 0.75))
-            self.mess_surface.set_alpha(150)
-            window.blit(self.mess_surface, (w / 2 - mw / 2, h / 2 - mh / 2))
-
-    def message_timer(self, counter):
-        Timer(0.2, self.message_switch, [counter]).start()
-
-    def message_switch(self, counter):
-        counter -= 1
-        if counter < 0:
-            self.messgo = False
-            return
-        if self.firstxt:
-            self.firstxt = False
-            self.secondtxt = True
-            self.message_timer(counter)
-        elif self.secondtxt:
-            self.secondtxt = False
-            self.thirdtxt = True
-            self.message_timer(counter)
-        elif self.thirdtxt:
-            self.thirdtxt = False
-            self.firstxt = True
-            self.message_timer(counter)
-
-
-    def score_card(self):
-        lives_write = str(self.lives)
-        if self.lives < 0:
-            lives_write = 0
-        scoresurface = self.myfont.render(f'Score: {str(self.score)}', False, (0, 0, 0))
-        livessurface = self.myfont.render(f'Lives: {str(lives_write)}', False, (0, 0, 0))
-        levelsurface = self.myfont.render(f'Level: {str(self.level)}', False, (0, 0, 0))
-        self.text_surface.fill((255, 255, 255))
-        self.text_surface.blit(scoresurface, (0, 0))
-        self.text_surface.blit(livessurface, (0, 20))
-        self.text_surface.blit(levelsurface, (0, 40))
-        self.text_surface.set_alpha(200)
-        window.blit(self.text_surface, (0, 0))
-
-    def speed_watcher(self, sall, enem):
-        if sall:
-            return len(bullets) * 0.06 + len(pups) * 0.1 + 1
-        else: 
-            if Gvar.level < 4:
-                return len(bullets) * 0.06 + len(pups) * 0.06 + 1
-            else: 
-                return len(bullets) * 0.06 + len(pups) * 0.06 + 3
-                
-    def pause(self, pauser):
-        all_arr = bullets + pups + drop_cat + fly_cat + enem_lis
-        pause_start = time.time()
-        self.paused = True
-        self.mus_switch = True
-        if self.pause_time != 0:
-            self.last_pause = self.pause_time
-
-        pause_font = pygame.font.SysFont('Arial', 35)
-        pause_surface = pygame.Surface((w - 100, h - 100))
-        pspw, psph = pause_surface.get_size()
-        pause_surface.set_alpha(120)
-        pspesc = pause_font.render('Paused, Enter/Esc to unpause', False, [255, 255, 255])
-        pspew, pspeh = pspesc.get_rect().size
-        while pauser:
-            window.blit(background_image, [0, 0])
-            Player.handle_spawn()
-            for Sprite in all_arr:
-                Sprite.handle_spawn()
-            self.score_card()
-            if self.level == 3:
-                self.show_message()
-
-            #Set Pause screen here
-            pause_surface.blit(pspesc, [pspw / 2 - pspew / 2, psph /2 - pspeh / 2])
-            window.blit(pause_surface, [(w - pspw) / 2, (h - psph) / 2])
-
-            pygame.display.update()
-            for event in pygame.event.get():
-                if (event.type == pygame.QUIT):
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_F4 and pygame.key.get_mods() & pygame.KMOD_ALT:
-                        pygame.quit()
-                        sys.exit()
-                    elif event.key == pygame.K_ESCAPE:
-                        pygame.mixer.music.unpause()
-                        pygame.mixer.unpause()
-                        self.paused = False
-                        self.pause_time = time.time() - pause_start
-                        Gvar.keys = {}
-                        pauser = False
-                    elif event.key == pygame.K_RETURN:
-                        pygame.mixer.music.unpause()
-                        pygame.mixer.unpause()
-                        self.paused = False
-                        self.pause_time = time.time() - pause_start
-                        Gvar.keys = {}
-                        pauser = False
 
 #Sprite Template
 class Sprite:
@@ -515,7 +333,8 @@ def global_inits():
         ]
 
     pygame.mixer.init()
-    Gvar = game_vars()
+    ng_vars = game_vars.game_vars
+    Gvar = ng_vars()
 
     #Set Player Sprite
     Player = Sprite('player', pather('./assets/player.png'), 0.4, [w / 2, h - 40], [60, 45])
@@ -645,7 +464,7 @@ def main():
                     pygame.mixer.music.pause()
                     pygame.mixer.pause()
                     Gvar.keys = {}
-                    Gvar.pause(True)
+                    Gvar.pause(True, bullets, pups, drop_cat, fly_cat, enem_lis, w, h, window, Player, background_image, Gvar)
                 elif event.key == pygame.K_ESCAPE:
                     pygame.mixer.music.stop()
                     pygame.mixer.stop()
@@ -673,7 +492,7 @@ def main():
             Player.spawn_drop(end)
             Player.spawn_fly(end)
             Player.stop_trip(end)
-        Player.movement(Gvar.keys.get('key_left', False), Gvar.keys.get('key_right', False), False, False, Gvar.speed_watcher(True, False))
+        Player.movement(Gvar.keys.get('key_left', False), Gvar.keys.get('key_right', False), False, False, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
         if Gvar.level < 3:
             Player.shooter(Gvar.keys.get('key_space'), False, Gvar.level)
         else: 
@@ -683,12 +502,12 @@ def main():
             if Player.triple_shoot and len(bullets) > 0:
                 bulli = Bullet.bull_place
                 if bulli == 1:
-                    Bullet.movement(True, False, False, True, Gvar.speed_watcher(True, False))
+                    Bullet.movement(True, False, False, True, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
                 elif bulli == 2:
-                    Bullet.movement(False, True, False, True, Gvar.speed_watcher(True, False))
+                    Bullet.movement(False, True, False, True, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
                 elif bulli == 3:
-                    Bullet.movement(False, False, False, True, Gvar.speed_watcher(True, False))
-            Bullet.movement(False, False, False, True, Gvar.speed_watcher(True, False))
+                    Bullet.movement(False, False, False, True, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
+            Bullet.movement(False, False, False, True, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
 
         for Drop in drop_cat:
             Drop.handle_spawn()
@@ -698,7 +517,7 @@ def main():
                 except:
                     print('drop_cat can\'t be popped')
                 Player.spawn_pups()
-            Drop.movement(False, False, True, False, Gvar.speed_watcher(False, True))
+            Drop.movement(False, False, True, False, Gvar.speed_watcher(False, True, bullets, pups, Gvar))
             
         for Fly in fly_cat:
             Fly.handle_spawn()
@@ -707,11 +526,11 @@ def main():
                     Gvar.lives += 1
                     if Gvar.level > 3:
                         Player.triple_shoot = True
-            Fly.movement(False, True, False, False, Gvar.speed_watcher(True, False))
+            Fly.movement(False, True, False, False, Gvar.speed_watcher(True, False, bullets, pups, Gvar))
 
         for Pup in pups:
             Pup.handle_spawn()
-            Pup.movement(False, True, False, False, Gvar.speed_watcher(False, True))
+            Pup.movement(False, True, False, False, Gvar.speed_watcher(False, True, bullets, pups, Gvar))
 
         for Enemies in enem_lis:
             # Enemies.check_collision(Bullet) #This may be different
@@ -721,12 +540,12 @@ def main():
                     Gvar.score += 1
             for Pup in pups:
                 Enemies.check_collision(Pup)
-            Enemies.movement(False, False, True, False, Gvar.speed_watcher(False, True))
+            Enemies.movement(False, False, True, False, Gvar.speed_watcher(False, True, bullets, pups, Gvar))
             
-        Gvar.score_card()
+        Gvar.score_card(window)
 
         if Gvar.level == 3:
-            Gvar.show_message()
+            Gvar.show_message(window, w, h)
 
         pygame.display.update()
         
---
+++ src/game_vars.py
@@ -0,0 +1,184 @@
+import pygame, sys, os, random, math, winsound, time
+from pygame.locals import *
+from threading import Timer
+
+class game_vars:
+        #Enemy Costumes
+    def __init__(self):
+        self.costumes = [
+            {
+                'img': 'bmouse.png',
+                'size': [40, 65]
+            },
+            {
+                'img': 'b-c.png',
+                'size': [40, 65]
+            },
+            {
+                'img': 'b-a.png',
+                'size': [40, 65]
+            },
+            {
+                'img': 'b-t.png',
+                'size': [40,65]
+            }
+        ]
+
+        #Game Vars
+        self.level = 1
+        self.score = 0
+        self.lives = 10
+        self.speed_enem = 1
+        self.speed_all = 1 
+        self.firing = True
+        self.keys = {}
+        self.game_loop = True
+        self.music = True
+        self.once = True
+        self.messgo = True
+        self.first_time = True
+        self.firstxt = True
+        self.secondtxt = False
+        self.thirdtxt = False
+        self.sscreen = True
+        self.pause_time = 0
+        self.last_pause = 0
+        self.mw_trans = 2.8
+        self.paused = False
+        self.messcolor = [0, 0, 0]
+        self.messfont = pygame.font.SysFont('Arial', 30)
+        self.mess_surface = pygame.Surface((300, 100))
+        self.myfont = pygame.font.SysFont('Arial', 20)
+        self.text_surface = pygame.Surface((100,75))
+
+
+    #Score Watching
+    def score_watcher(self, sc):
+        if sc < 20:
+            return 1
+        elif sc >= 20 and sc <= 29:
+            return 2
+        elif sc >= 30 and sc <= 49:
+            return 3
+        elif sc >= 50:
+            return 4
+
+    def show_message(self, window, w, h):
+        if self.messgo:
+            if self.first_time:
+                self.first_time = False
+                Timer(0.2, self.message_timer, [20]).start()
+            if self.firstxt:
+                self.messcolor = [255, 255, 255]
+            elif self.secondtxt:
+                self.messcolor = [255, 153, 229]
+            elif self.thirdtxt:
+                self.messcolor = [0, 255, 255]
+            message = self.messfont.render('Automatic Fire Mode', False, self.messcolor)
+            spaceage = self.messfont.render('(Hold Spacebar)', False, self.messcolor)
+            tw, th = message.get_rect().size
+            sw, sh = spaceage.get_rect().size
+            mw, mh = self.mess_surface.get_rect().size
+            
+            
+            self.mess_surface.fill((0, 0, 0))
+            self.mess_surface.blit(message, (mw / 2 - tw / 2, mh * 0.25 - th * 0.25))
+            self.mess_surface.blit(spaceage, (mw * 0.5 - sw * 0.5, mh * 0.75 - sh * 0.75))
+            self.mess_surface.set_alpha(150)
+            window.blit(self.mess_surface, (w / 2 - mw / 2, h / 2 - mh / 2))
+
+    def message_timer(self, counter):
+        Timer(0.2, self.message_switch, [counter]).start()
+
+    def message_switch(self, counter):
+        counter -= 1
+        if counter < 0:
+            self.messgo = False
+            return
+        if self.firstxt:
+            self.firstxt = False
+            self.secondtxt = True
+            self.message_timer(counter)
+        elif self.secondtxt:
+            self.secondtxt = False
+            self.thirdtxt = True
+            self.message_timer(counter)
+        elif self.thirdtxt:
+            self.thirdtxt = False
+            self.firstxt = True
+            self.message_timer(counter)
+
+
+    def score_card(self, window):
+        lives_write = str(self.lives)
+        if self.lives < 0:
+            lives_write = 0
+        scoresurface = self.myfont.render(f'Score: {str(self.score)}', False, (0, 0, 0))
+        livessurface = self.myfont.render(f'Lives: {str(lives_write)}', False, (0, 0, 0))
+        levelsurface = self.myfont.render(f'Level: {str(self.level)}', False, (0, 0, 0))
+        self.text_surface.fill((255, 255, 255))
+        self.text_surface.blit(scoresurface, (0, 0))
+        self.text_surface.blit(livessurface, (0, 20))
+        self.text_surface.blit(levelsurface, (0, 40))
+        self.text_surface.set_alpha(200)
+        window.blit(self.text_surface, (0, 0))
+
+    def speed_watcher(self, sall, enem, bullets, pups, Gvar):
+        if sall:
+            return len(bullets) * 0.06 + len(pups) * 0.1 + 1
+        else: 
+            if Gvar.level < 4:
+                return len(bullets) * 0.06 + len(pups) * 0.06 + 1
+            else: 
+                return len(bullets) * 0.06 + len(pups) * 0.06 + 3
+                
+    def pause(self, pauser, bullets, pups, drop_cat, fly_cat, enem_lis, w, h, window, Player, background_image, Gvar):
+        all_arr = bullets + pups + drop_cat + fly_cat + enem_lis
+        pause_start = time.time()
+        self.paused = True
+        self.mus_switch = True
+        if self.pause_time != 0:
+            self.last_pause = self.pause_time
+
+        pause_font = pygame.font.SysFont('Arial', 35)
+        pause_surface = pygame.Surface((w - 100, h - 100))
+        pspw, psph = pause_surface.get_size()
+        pause_surface.set_alpha(120)
+        pspesc = pause_font.render('Paused, Enter/Esc to unpause', False, [255, 255, 255])
+        pspew, pspeh = pspesc.get_rect().size
+        while pauser:
+            window.blit(background_image, [0, 0])
+            Player.handle_spawn()
+            for Sprite in all_arr:
+                Sprite.handle_spawn()
+            self.score_card(window)
+            if self.level == 3:
+                self.show_message(window, w, h)
+
+            #Set Pause screen here
+            pause_surface.blit(pspesc, [pspw / 2 - pspew / 2, psph /2 - pspeh / 2])
+            window.blit(pause_surface, [(w - pspw) / 2, (h - psph) / 2])
+
+            pygame.display.update()
+            for event in pygame.event.get():
+                if (event.type == pygame.QUIT):
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_F4 and pygame.key.get_mods() & pygame.KMOD_ALT:
+                        pygame.quit()
+                        sys.exit()
+                    elif event.key == pygame.K_ESCAPE:
+                        pygame.mixer.music.unpause()
+                        pygame.mixer.unpause()
+                        self.paused = False
+                        self.pause_time = time.time() - pause_start
+                        Gvar.keys = {}
+                        pauser = False
+                    elif event.key == pygame.K_RETURN:
+                        pygame.mixer.music.unpause()
+                        pygame.mixer.unpause()
+                        self.paused = False
+                        self.pause_time = time.time() - pause_start
+                        Gvar.keys = {}
+                        pauser = False
